c2dd062a8a1fb0a27a17dad3b211414be658345a
Matias Urra <matiexe31@gmail.com>
2019-09-04 16:01:50 -0300

    cambio en config

--- server/config/config.js
+++ server/config/config.js
@@ -9,10 +9,10 @@ const port = process.env.PORT || 3000;
 
 process.env.NODE_ENV = process.env.NODE_ENV ||'dev';
 let urlDB;
-if (process.env.NODE_ENV = 'dev'){
-   urlDB ='mongodb://localhost:27017/game';
-}else{
+//if (process.env.NODE_ENV = 'dev'){
+  // urlDB ='mongodb://localhost:27017/game';
+//}else{
     urlDB =process.env.MONGO_URI;
-}
+//}
 
 process.env.URLDB = urlDB;
